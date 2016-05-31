4949c66534d8388d203a9fa8afd68cc415425a63
fromseto <wanglin44@gmail.com>
2016-05-31 17:34:49 -0400

    create upload template
    Neeed SQL to update remote database, this is issue #10

---
+++ metrxnWeb2014/js/upload.js
@@ -0,0 +1,194 @@
+var inputType = "string";
+var stepped = 0, rowCount = 0, errorCount = 0, firstError;
+var start, end;
+var firstRun = true;
+var maxUnparseLength = 10000;
+
+$(function()
+{
+	// Tabs
+	$('#submit').text("Parse");
+	inputType = "local";
+
+
+
+
+	// Sample files
+	$('#remote-normal-file').click(function() {
+		$('#url').val($('#local-normal-file').attr('href'));
+	});
+	$('#remote-large-file').click(function() {
+		$('#url').val($('#local-large-file').attr('href'));
+	});
+	$('#remote-malformed-file').click(function() {
+		$('#url').val($('#local-malformed-file').attr('href'));
+	});
+
+
+
+
+	// Demo invoked
+	$('#submit').click(function()
+	{
+		if ($(this).prop('disabled') == "true")
+			return;
+
+		stepped = 0;
+		rowCount = 0;
+		errorCount = 0;
+		firstError = undefined;
+
+		var config = buildConfig();
+		var input = $('#input').val();
+
+		if (inputType == "remote")
+			input = $('#url').val();
+		else if (inputType == "json")
+			input = $('#json').val();
+
+		// Allow only one parse at a time
+		$(this).prop('disabled', true);
+
+		if (!firstRun)
+			console.log("--------------------------------------------------");
+		else
+			firstRun = false;
+
+
+
+		if (inputType == "local")
+		{
+			if (!$('#files')[0].files.length)
+			{
+				alert("Please choose at least one file to parse.");
+				return enableButton();
+			}
+			
+			$('#files').parse({
+				config: config,
+				header: true,
+				before: function(file, inputElem)
+				{
+					start = now();
+					console.log("Parsing file...", file);
+				},
+				error: function(err, file)
+				{
+					console.log("ERROR:", err, file);
+					firstError = firstError || err;
+					errorCount++;
+				},
+				complete: function()
+				{
+					end = now();
+					printStats("Done with all files");
+				}
+			});
+		}
+		else
+		{
+			start = now();
+			var results = Papa.parse(input, config);
+			console.log("Synchronous results:", results);
+			if (config.worker || config.download)
+				console.log("Running...");
+		}
+	});
+
+	$('#insert-tab').click(function()
+	{
+		$('#delimiter').val('\t');
+	});
+});
+
+
+
+
+function printStats(msg)
+{
+	if (msg)
+		console.log(msg);
+	console.log("       Time:", (end-start || "(Unknown; your browser does not support the Performance API)"), "ms");
+	console.log("  Row count:", rowCount);
+	if (stepped)
+		console.log("    Stepped:", stepped);
+	console.log("     Errors:", errorCount);
+	if (errorCount)
+		console.log("First error:", firstError);
+}
+
+
+
+function buildConfig()
+{
+	return {
+		delimiter: $('#delimiter').val(),
+		header: true,
+		dynamicTyping: $('#dynamicTyping').prop('checked'),
+		skipEmptyLines: $('#skipEmptyLines').prop('checked'),
+		preview: parseInt($('#preview').val() || 0),
+		step: $('#stream').prop('checked') ? stepFn : undefined,
+		encoding: $('#encoding').val(),
+		worker: $('#worker').prop('checked'),
+		comments: $('#comments').val(),
+		complete: completeFn,
+		error: errorFn,
+		download: inputType == "remote"
+	};
+}
+
+function stepFn(results, parser)
+{
+	stepped++;
+	if (results)
+	{
+		if (results.data)
+			rowCount += results.data.length;
+		if (results.errors)
+		{
+			errorCount += results.errors.length;
+			firstError = firstError || results.errors[0];
+		}
+	}
+}
+
+function completeFn(results)
+{
+	end = now();
+
+	if (results && results.errors)
+	{
+		if (results.errors)
+		{
+			errorCount = results.errors.length;
+			firstError = results.errors[0];
+		}
+		if (results.data && results.data.length > 0)
+			rowCount = results.data.length;
+	}
+
+	printStats("Parse complete");
+	console.log("    Results:", results);
+
+	// icky hack
+	setTimeout(enableButton, 100);
+}
+
+function errorFn(err, file)
+{
+	end = now();
+	console.log("ERROR:", err, file);
+	enableButton();
+}
+
+function enableButton()
+{
+	$('#submit').prop('disabled', false);
+}
+
+function now()
+{
+	return typeof window.performance !== 'undefined'
+			? window.performance.now()
+			: 0;
+}
